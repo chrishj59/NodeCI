b5972be5430550ac09c0a8b67d6981b59be83db5
chrishj59 <chrishj@btinternet.com>
2020-02-29 16:30:48 +0000

    add save images in AWS s3

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -1,30 +1,44 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchBlog } from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchBlog } from "../../actions";
 
 class BlogShow extends Component {
-  componentDidMount() {
-    this.props.fetchBlog(this.props.match.params._id);
-  }
+	componentDidMount() {
+		this.props.fetchBlog(this.props.match.params._id);
+	}
 
-  render() {
-    if (!this.props.blog) {
-      return '';
-    }
+	renderImage() {
+		if (this.props.blog.imageUrl) {
+			return (
+				<img
+					src={
+						"https://rationem-node-1.s3.eu-west-2.amazonaws.com" +
+						this.props.blog.imageUrl
+					}
+					alt="blog"
+				/>
+			);
+		}
+	}
+	render() {
+		if (!this.props.blog) {
+			return "";
+		}
 
-    const { title, content } = this.props.blog;
+		const { title, content } = this.props.blog;
 
-    return (
-      <div>
-        <h3>{title}</h3>
-        <p>{content}</p>
-      </div>
-    );
-  }
+		return (
+			<div>
+				<h3>{title}</h3>
+				<p>{content}</p>
+				{this.renderImage()}
+			</div>
+		);
+	}
 }
 
 function mapStateToProps({ blogs }, ownProps) {
-  return { blog: blogs[ownProps.match.params._id] };
+	return { blog: blogs[ownProps.match.params._id] };
 }
 
 export default connect(mapStateToProps, { fetchBlog })(BlogShow);
---
+++ routes/uploadRoutes.js
@@ -0,0 +1,26 @@
+const AWS = require("aws-sdk");
+//const uuid = require("uuid/v1");
+const { v4: uuidv4 } = require("uuid");
+const requireLogin = require("../middlewares/requireLogin");
+const keys = require("../config/keys");
+
+const s3 = new AWS.S3({
+	accessKeyId: keys.accessKeyId,
+	secretAccessKey: keys.secretAccessKey
+});
+
+module.exports = app => {
+	app.get("/api/upload", requireLogin, (req, res) => {
+		const key = `${req.user.id}/${uuidv4()}.jpeg`;
+
+		s3.getSignedUrl(
+			"putObject",
+			{
+				Bucket: "rationem-node-1",
+				ContentType: "image/jpeg",
+				Key: key
+			},
+			(err, url) => res.send({ key, url })
+		);
+	});
+};
